1ed0cc5345a3e3c3c9481297fd8bee8e66788426
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2022-06-25 15:33:10 +0100

    Status Element

--- js/main.js
+++ js/main.js
@@ -28,3 +28,59 @@ let users = [
     registrationNumber: "3576",
   },
 ];
+
+/* ////////// handle show & hid modal /////////// */
+const modal = document.querySelector(".modal");
+const addUserBtn = document.querySelector(".add-user-card .button");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+addUserBtn.addEventListener("click", (event) => {
+  modal.classList.add("show");
+  modalOverlay.classList.add("show");
+});
+
+modalOverlay.addEventListener("click", (event) => {
+  modal.classList.remove("show");
+  modalOverlay.classList.remove("show");
+});
+
+//Add & delete & show users
+
+const usersList = document.querySelector("table.users-table > tbody");
+console.log(usersList);
+
+const showUsers = (users) => {
+  const usersList = document.querySelector("table.users-table > tbody");
+
+  users.forEach((user) => {
+    usersList.innerHTML += /*html*/ `
+          <tr>
+            <td>${user.id}</td>
+            <td>${new Date(user.createdDate).toLocaleDateString()}</td>
+            <td>
+            <span class="status ${
+              user.status === "En validation"
+                ? "on-validation"
+                : user.status === "Validé"
+                ? "valide"
+                : user.status === "Rejeté"
+                ? "rejected"
+                : ""
+            }">
+              ${user.status}
+            </span> 
+            </td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.userName}</td>
+            <td>${user.registrationNumber}</td>
+            <td><i class="fas fa-trash-alt" id="remove"></i></td>
+          </tr>
+    
+        `;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  showUsers(users);
+});
